3531ec43378a3317fe8739f4d124e0a6f696a2fd
王江 <wangjia@qq.com>
2020-05-16 10:51:07 +0800

    feature(channel): 完善新增频道页

--- src/app/containers/addChannel/index.js
+++ src/app/containers/addChannel/index.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import './index.scss'
 import { Link } from 'react-router-dom'
+import Switch from '@/plugin/components/switch/index'
 export default class index extends Component {
     constructor(props) {
         super(props)
         this.state = {
+            channelName: "",
             placeholder: "给频道取个名字",
             publicChannelDesc: "空间内所有成员可查看并自由加入该频道",
             privateChannelDesc: "仅被邀请人可见"
 
         }
+        this.channelNameChange = this.channelNameChange.bind(this);
+    }
+    channelNameChange(e) {
+        e.preventDefault()
+        this.setState({ channelName: e.target.value })
     }
     render() {
         return (
@@ -21,13 +28,28 @@ export default class index extends Component {
                 </div>
                 <div className="main">
                     <div className="input">
-                        <input type="text" name="channelName" className="input-channel-name" autoFocus placeholder={this.state.placeholder}></input>
+                        <input type="text" name="channelName" value={this.state.channelName} className="input-channel-name" autoFocus placeholder={this.state.placeholder} onChange={this.channelNameChange} autoComplete="off"></input>
+                        {this.state.channelName &&
+                            <div className="next">
+                                <div className="iconfont iconarrow_right"></div>
+                            </div>
+                        }
                     </div>
                     <div className="msg">
+                        <div className="switch">
+                            <Switch></Switch>
+                        </div>
                         <div className="desc">
                             {this.state.publicChannelDesc}
                         </div>
                     </div>
+                    {this.state.channelName &&
+                        <div className="suggestion">
+                            <div className="desc">
+                                您是否想加入以下公开频道
+                            </div>
+                        </div>
+                    }
                 </div>
             </div>
         );
