389462d1bd993ba01c6c114cfbc9e1d967ab0e2f
王江 <wangjia@qq.com>
2020-07-16 18:02:32 +0800

    feature(channle) 增加draft编辑器

--- src/app/containers/channel/main/cardOverview.js
+++ src/app/containers/channel/main/cardOverview.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Editor, EditorState,convertToRaw,convertFromRaw } from 'draft-js';
+import { Editor, EditorState, convertToRaw, convertFromRaw } from 'draft-js';
 import './cardOverview.scss'
 
 class Channels extends React.Component {
@@ -13,14 +13,14 @@ class Channels extends React.Component {
     }
     onChange = (editorState, module) => {
         this.setState({ editorState, curModuleId: module.id });
-        
-       
+
+
         let content = editorState.getCurrentContent()
         this.saveRichText(convertToRaw(content))
     };
     saveRichText = async (content) => {
         if (!this.state.curModuleId) return
-        let res = await React.$request.post(`/dafang/richText/create/${this.state.curModuleId}`, { content: JSON.stringify(content) })
+        let res = await React.$request.post(`/dafang/richText/createOrUpdate/${this.state.curModuleId}`, { content: JSON.stringify(content) })
     }
     componentDidMount() {
         React.$store.subscribe(this.getModules)
@@ -31,9 +31,8 @@ class Channels extends React.Component {
             let res = await React.$request.get(`/dafang/module/panel/${activedPanel.id}`)
             this.setState({ modules: res.data })
             this.state.modules.map(async m => {
-                if (m.type === 1) {
-                    let content = await this.getModuleContent(m.id)
-                    // console.log(content)
+                let content = await this.getModuleContent(m.id)
+                if (m.type === 1 && content) {
                     this.setState({ editorState: EditorState.createWithContent(convertFromRaw(content)) })
                     // EditorState.push(this.state.editorState, content)
                 }
@@ -43,7 +42,7 @@ class Channels extends React.Component {
 
     getModuleContent = async (id) => {
         let res = await React.$request.get(`/dafang/richText/module/${id}`)
-        return res.data?JSON.parse(res.data.content):{}
+        return res.data ? JSON.parse(res.data.content) : ''
     }
     render() {
         let { showPre } = this.props
