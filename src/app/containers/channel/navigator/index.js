f9f4083fbeee28491b0be0fdae77fa4c0b95bbe1
chenlibiti <chenlibiti@163.com>
2020-08-14 17:31:55 +0800

    删除面板

--- src/app/containers/channel/navigator/index.js
+++ src/app/containers/channel/navigator/index.js
@@ -1,5 +1,7 @@
+/* eslint-disable default-case */
 import './index.scss';
 import React, { Component } from 'react';
+import RightClickMenu from '../main/rightClickMenu'
 
 class Navi extends Component {
     constructor(props) {
@@ -9,7 +11,11 @@ class Navi extends Component {
             panels: [],
             activedPanel: 0,
             addPanel: false,
-            panelName:''
+            panelName:'',
+            clientX: 0,
+            clientY: 0,
+            showRightclickMenu: false,
+            deletePanelOrder: -1
         }
     }
     componentDidMount() {
@@ -49,6 +55,38 @@ class Navi extends Component {
     changePanelName(e){
         this.setState({ panelName: e.target.value })
     }
+    toggleShowRightClickMenu = () => {
+        this.setState({
+            showRightclickMenu: false,
+            deletePanelOrder: -1
+        })
+    }
+    //右键菜单
+    contextMenu = e => {
+        e.preventDefault();
+        e.stopPropagation()
+        let { clientX, clientY } = e
+        this.setState({ clientX, clientY })
+        this.setState({ showRightclickMenu: !this.state.showRightclickMenu })
+        this.setState({ deletePanelOrder: e.target.id})
+    }
+    // 右键菜单点击
+    rcBack = (e) => {
+        switch (e) {
+            case 'del': this.delPanel()
+        }
+
+    }
+
+    delPanel = async () => {
+       
+        let panelList = this.state.panels;
+        let start = parseInt(this.state.deletePanelOrder)
+        let delPanel = panelList.splice(start,1)
+        let res = await React.$request.delete(`/dafang/panel/${delPanel[0].id}`)
+        this.setState({ panels: panelList })
+        this.toggleShowRightClickMenu()
+    }
 
     handleEnterKey = async (e) => {
       
@@ -67,19 +105,25 @@ class Navi extends Component {
         }
     }
     render() {
-        const { panels, activedPanel, addPanel, panelName} = this.state
+        const { panels, activedPanel, addPanel, panelName, clientX, clientY, showRightclickMenu} = this.state
         return (
             <div className="wrap-navigator">
                 <div className="left">
                     {
                         panels.map((panel, index) =>
-                            <div className={`cursor item ${index === activedPanel && 'active'}`} key={index} onClick={e => this.setActivedPanel(index)}>{panel.name}</div>
+                            <div className={`cursor item ${index === activedPanel && 'active'}`} id={index} key={index} onClick={e => this.setActivedPanel(index)} onContextMenu={(e) => this.contextMenu(e)}>{panel.name}
+                                
+                            </div>
                         )
                     }
                     <div className="add-panel-div" style={{ display: addPanel ? 'block' : 'none' }} key='add-panel' > 
                         <input type="text" className="add-panel-input" name="panelName" value={panelName} autoFocus autoComplete="off" placeholder="未命名面板" onChange={(e) => this.changePanelName(e)} onKeyPress={(e) => this.handleEnterKey(e)}></input>
                     </div>
                     <div className="iconfont iconaddboard pointer" onClick={(e) => this.changeAddPanel(e)}></div>
+                    <div className="right-click-menu" style={{ left: clientX + 5 + "px", top: clientY + 5 + "px" }}>
+                        {<RightClickMenu show={showRightclickMenu} rcBack={e => this.rcBack(e)} toggleShow={() => { this.toggleShowRightClickMenu() }} ></RightClickMenu>}
+                    </div>
+
                 </div>
                 <div className="wrap-navigator-right">
                     <div className="iconfont iconchannel_search"></div>
